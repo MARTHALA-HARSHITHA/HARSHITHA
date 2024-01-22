e6710a7c16a2993eab7009afbbbdf95a44e294bd
MARTHALA-HARSHITHA <157340515+MARTHALA-HARSHITHA@users.noreply.github.com>
2024-01-22 10:31:04 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello World !</h1>
+      <h2> Roll No : 2211cs010365</h2>
+      <h2> Name : M.Harshitha</h2>
+      <h2> CGPA : 9.02</h2>
+      
+      
+    </div>
+    
+
+   
+  );
+}
+
+export default App;
